75b949eaabfa9a7a31ea71e08551ae5eec543a2d
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-08 18:24:31 +0200

    Example of how ngOnDestroy works

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,18 +1,19 @@
 import { Product } from './../product';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent implements OnInit {
+export class ProductItemComponent implements OnDestroy {
 
   //This @Input() tells Angular that we want to resend this product as a variable. Check the product-list.component.html to see how I call it
   @Input() product: Product;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    console.log("ProductItemComponent - OnDestroy");
   }
 
 }
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
 
@@ -7,15 +7,25 @@ import { ProductsService } from '../products.service';
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
   products: Array<Product>;
 
   constructor(private productsService: ProductsService) {
   }
 
+  //it is called when our component is opened
   ngOnInit(): void {
     console.log('ProductsListComponent - ngOnInit');
     this.products = this.productsService.getProducts();
     console.log('ProductsListComponent - products', this.products);
+  }
+
+  deleteProducts(): void {
+    this.products = [];
+  }
+
+  //it is called when our component is closed
+  ngOnDestroy(): void{
+
   }
 }

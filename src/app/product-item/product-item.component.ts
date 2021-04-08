37d939525557e23aea17849c143d590b71be61c4
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-08 18:30:30 +0200

    Example of a memory leak

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,16 +1,23 @@
 import { Product } from './../product';
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent implements OnDestroy {
+export class ProductItemComponent implements OnInit, OnDestroy {
 
   //This @Input() tells Angular that we want to resend this product as a variable. Check the product-list.component.html to see how I call it
   @Input() product: Product;
   constructor() { }
+
+  //even after we delete the items, this will ngOnInit will continue to console log every 5 seconds
+  ngOnInit(): void{
+    setInterval(() => {
+      console.log("ProductItemComponent - ngOnInit");
+    }, 5000);
+  }
 
   ngOnDestroy(): void {
     console.log("ProductItemComponent - OnDestroy");

2958fc4c868bbb9bd9696a8dc4e10eb65d1eace7
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-08 17:22:58 +0200

    Making an export class and a service

---
+++ src/app/products.service.ts
@@ -0,0 +1,20 @@
+import { Product } from './product';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  private products = [
+    {id: '1', name: 'Shampoo', price: 13.99, imageUrl: 'https://via.placeholder.com/300'},
+    {id: '2', name: 'Deodorant', price: 6.58, imageUrl: 'https://via.placeholder.com/300'},
+    {id: '3', name: 'Toothbrush', price: 10.99, imageUrl: 'https://via.placeholder.com/300'},
+    // {id: '4', name: 'Toothpaste', price: 15, imageUrl: 'https://via.placeholder.com/300'}
+  ];
+
+  constructor() { }
+
+  getProducts(): Array<Product>{
+    return this.products;
+  }
+}

5017c2b0b5343112a0ed2a1eae8dbeef786d2955
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-08 18:09:54 +0200

    New item component to separate from the list

---
+++ src/app/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Product } from './../product';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  //This @Input() tells Angular that we want to resend this product as a variable. Check the product-list.component.html to see how I call it
+  @Input() product: Product;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

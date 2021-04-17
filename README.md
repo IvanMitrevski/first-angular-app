ccbc0bdd4f62b104f5e4f4026581eb90f68b01a2
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-04-17 20:04:08 +0200

    Learned about pipes

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - Learned about lifecycle hooks and parent-child communication.
 - Learned about attribute and structural directives.
+- Learned about pipes (lowercase,uppercase,slice,date)
 
 Angular is fun :)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
-  color: string;
+  color: any;
+  today: number = Date.now();
+  public product =  { id: '5', name: 'Rakija', price: 499.99, imageUrl: 'https://wine.mk/wp-content/uploads/2020/04/qwer.png' };
 }
